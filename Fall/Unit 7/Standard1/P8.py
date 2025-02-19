c5249fc4cf0c68e6264535791f35d308bc7e40ac
Foojanb <babaeeianwork@gmail.com>
2025-02-18 17:51:31 -0800

    session 1

---
+++ Fall/Unit 7/Standard1/P8.py
@@ -0,0 +1,31 @@
+class Node:
+    def __init__(self, value, next=None):
+        self.value = value
+        self.next = next
+
+def merge_missions(mission1, mission2):
+    if mission1 is None:
+        return mission2
+    if mission2 is None:
+        return mission1
+
+    if mission1.value <= mission2.value:
+        mission1.next = merge_missions(mission1.next, mission2)
+        return mission1
+    else:
+        mission2.next = merge_missions(mission1, mission2.next)
+        return mission2
+
+def print_linked_list(head):
+    current = head
+    while current:
+        print(current.value, end=" -> " if current.next else "\n")
+        current = current.next
+
+# mission1 = Node(1, Node(2, Node(4)))
+# mission2 = Node(1, Node(3, Node(4, Node(5))))
+mission1 = None
+mission2 = None
+
+merged_head = merge_missions(mission1, mission2)
+print_linked_list(merged_head)
---
+++ Spring/Unit 1/st-1/problems.py
@@ -0,0 +1,21 @@
+# problem 1
+def welcome():
+	print("Welcome to The Hundred Acre Wood!")
+
+# problem 2
+def greeting(name):
+	print(f"Welcome to The Hundred Acre Wood {name}! My name is Fozhan Babaeiyan.")
+
+# problem 3
+def print_catchphrase(character):
+	if character == "Pooh":
+		print("Oh bother!")
+	elif character == "Tigger":
+		print("TTFN: Ta-Ta for now!")
+	elif character == "Eeyore":
+		print("Thanks for noticing me.")
+	elif character == "Christopher Robin":
+		print("Silly old bear.")
+	else:
+		print("Sorry! I don't know Piglet's catchphrase!"
+)
